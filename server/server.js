12ad8bcaa18943108c616aa1f02856185bd6230c
Raghuveer Singh <rajputraghu254@gmail.com>
2025-12-01 13:36:06 +0530

    node_module changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
+import 'dotenv/config'
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
